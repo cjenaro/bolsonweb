685e1726777c6623d58d95d5bcec77b3f587704b
root <root@LAPTOP-BESF9BVO.localdomain>
2019-02-12 19:16:13 -0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-menu mode="horizontal">
+      <el-menu-item index="1"><router-link to="/">Inicio</router-link></el-menu-item>
+      <el-menu-item index="2"><router-link to="/turismo">Turismo</router-link></el-menu-item>
+      <el-menu-item index="3"><router-link to="/cultura">Cultura</router-link></el-menu-item>
+    </el-menu>
+    <Home/>
   </div>
 </template>
 
-<style lang="scss">
+<script>
+import Home from './views/Home.vue'
+import Turismo from './views/Turismo.vue'
+import Cultura from './views/Cultura.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Home,
+    Turismo,
+    Cultura,
+  }
+}
+</script>
+
+<style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
